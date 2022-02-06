08629ca00aa2cfc435652e623cdcb5743e770121
Danil Beltyukov <beltyukov.e.danil@gmail.com>
2022-02-06 21:39:52 +0300

    Тестирование работы модели-представления главного окна

---
+++ IISBsuir/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Media;
+using IISBsuir.ViewModels.Base;
+
+namespace IISBsuir.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string _Title = "IIS Bsuir";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+
+    }
+}
