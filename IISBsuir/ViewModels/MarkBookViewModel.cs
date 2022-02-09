280897edeae627a2d421b6ead4bfc6535a9c785b
Danil Beltyukov <beltyukov.e.danil@gmail.com>
2022-02-10 01:54:47 +0300

    Базовая модель-представление MarkBook

---
+++ IISBsuir/ViewModels/MarkBookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using IISBsuir.ViewModels.Base;
+
+namespace IISBsuir.ViewModels
+{
+    internal class MarkBookViewModel : ViewModel
+    {
+        public MainWindowViewModel MainModel { get; }
+
+        public MarkBookViewModel(MainWindowViewModel mainWindowModel)
+        {
+            MainModel = mainWindowModel;
+        }
+    }
+}
