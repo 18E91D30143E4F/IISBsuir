06bc331d1039293e1894d81d82ea4f30a25662b9
Danil Beltyukov <beltyukov.e.danil@gmail.com>
2022-02-10 02:01:07 +0300

    Добавлено свойство ""

--- IISBsuir/ViewModels/MarkBookViewModel.cs
+++ IISBsuir/ViewModels/MarkBookViewModel.cs
@@ -1,4 +1,7 @@
-﻿using IISBsuir.ViewModels.Base;
+﻿using System.Threading.Tasks;
+using IISBsuir.Services;
+using IISBsuir.ViewModels.Base;
+using IIsHelper.Models;
 
 namespace IISBsuir.ViewModels
 {
@@ -6,8 +9,31 @@ namespace IISBsuir.ViewModels
     {
         public MainWindowViewModel MainModel { get; }
 
+        private DataService _dataService;
+
+        #region Список отметок по семестрам
+
+        private MarkBook _markBook;
+
+        public MarkBook MarkBook
+        {
+            get => _markBook;
+            set => Set(ref _markBook, value);
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Отладочный конструктор
+        /// </summary>
+        public MarkBookViewModel() : this(null)
+        {
+            MarkBook = Task.Run(_dataService.BsuirClient.GetMarkBookAsync).Result;
+        }
+
         public MarkBookViewModel(MainWindowViewModel mainWindowModel)
         {
+            _dataService = Task.Run(DataService.GetInstance).Result;
             MainModel = mainWindowModel;
         }
     }
